7d399b3f52dee63f10c02f917ef317c29f5ad041
Sam Chittenden <sbchittenden@gmail.com>
2016-09-01 11:00:22 -0600

    Updated credit card form and validation js

--- credit_card_form/credit.js
+++ credit_card_form/credit.js
@@ -11,6 +11,7 @@ var creditCardNumber = document.getElementById('cc_number');
 var cvcCode = document.getElementById('cvc');
 var expMonth = document.getElementById('expiration_month');
 var expYear = document.getElementById('expiry_year');
+var error = document.querySelectorAll('.error');
 
 // add event listener for ccForm submission
 ccForm.addEventListener('submit', function(event) {
@@ -23,37 +24,33 @@ ccForm.addEventListener('submit', function(event) {
     ------------- */
     // full name should not be empty
     if (validator.isEmpty(fullName.value)) {
+        error[0].innerHTML = "Oops! Please enter your full name."
+        error[0].className = "error active";
+        fullName.parentNode.classList.add("invalid");
+    } else if (!validator.isTrimmed(fullName.value)) {
+        error[0].innerHTML = "Please don't include any extra whitespace."
+        error[0].className = "error active";
         fullName.parentNode.classList.add("invalid");
     } else {
         fullName.parentNode.classList.remove("invalid");
-    }
-
-    // full name should contain leading or trailing whitespace
-    if (!validator.isTrimmed(fullName.value)) {
-        fullName.parentNode.classList.add("invalid");
-    } else {
-        fullName.parentNode.classList.remove("invalid");
+        ccForm.className = "valid";
     }
 
     /*-----------------
     validation for credit card number input field  
     -------------------*/
-
-    // cc number should not be empty
+    // cc number field should not be empty
     if (validator.isEmpty(creditCardNumber.value)) {
+        error[1].innerHTML = "Oh oh! Please enter a card number."
+        error[1].className = "error active";
+        creditCardNumber.parentNode.classList.add("invalid");
+    } else if (!validator.isCreditCard(creditCardNumber.value)) {
+        error[1].innerHTML = "Doh! Your card number doesn't look right..."
+        error[1].className = "error active";
         creditCardNumber.parentNode.classList.add("invalid");
     } else {
         creditCardNumber.parentNode.classList.remove("invalid");
+        ccForm.className = "valid";
     }
-
-    // cc number should pass isCreditCard 
-    if (!validator.isCreditCard(creditCardNumber.value)) {
-        creditCardNumber.parentNode.classList.add("invalid");
-    } else {
-        creditCardNumber.parentNode.classList.remove("invalid");
-    }
-
-
-
 
 });
