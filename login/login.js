400ac9a46e10b9b4056c8d5848a10025c08a147a
Sam Chittenden <sbchittenden@gmail.com>
2016-09-01 12:37:32 -0600

    Updated login.css and login.html, added login.js

---
+++ login/login.js
@@ -0,0 +1,58 @@
+//////////////////
+// Log in form  // 
+//////////////////
+
+// retrieve log in form elements
+var loginForm = document.getElementById("login_form");
+var userName = document.getElementById("username");
+var password = document.getElementById("password");
+var error = document.querySelectorAll(".error");
+
+// set event listener for login form submission
+loginForm.addEventListener('submit', function(event) {
+    // stop the event from its default action: submitting the form (for our validation, submission is not desired)
+    event.preventDefault();
+
+    /*------------------------------
+    validation for username field
+    ------------------------------*/
+
+    // username should not be empty
+    if (validator.isEmpty(userName.value)) {
+      error[0].innerHTML = "Hey Buddy! Even bacon lovers need to enter a user name.";
+      error[0].className = "error active";
+      userName.className = "invalid";
+    // username should not have any extra whitespace
+    } else if (!validator.isTrimmed(userName.value)) {
+      error[0].innerHTML = "Ummm... Your user name shouldn't have any extra whitespace added.";
+      error[0].className = "error active";
+      userName.className = "invalid";
+    // username should be alphanumeric
+    } else if (!validator.isAlphanumeric(userName.value)) {
+      error[0].innerHTML = "Whoa there... Your user name shouldn't have any special characters in it.";
+      error[0].className = "error active";
+      userName.className = "invalid";
+    }
+
+    /*---------------------------------
+    validation for password field
+    -----------------------------------*/
+
+    // password should not be empty
+    if (validator.isEmpty(password.value)) {
+      error[1].innerHTML = "Oops! Thoughts of bacon must have made you forget to enter a password.";
+      error[1].className = "error active";
+      password.className = "invalid";
+    // password should not have any extra whitespace
+    } else if (!validator.isTrimmed(password.value)) {
+      error[1].innerHTML = "Ummm... Your password shouldn't have any extra whitespace added.";
+      error[1].className = "error active";
+      password.className = "invalid";
+    // password should not contain any spaces
+    } else if (!validator.contains(password.value, [' '])) {
+      error[1].innerHTML = "Doh! Your password shouldn't contain any spaces.";
+      error[1].className = "error active";
+      password.className = "invalid";
+    }
+
+});
