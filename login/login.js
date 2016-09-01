ecdd2488bbc9288e64873925caf8c1d5838ec35d
Sam Chittenden <sbchittenden@gmail.com>
2016-09-01 13:05:30 -0600

    Updated login.js
    Added event listeners for input

--- login/login.js
+++ login/login.js
@@ -8,7 +8,7 @@ var userName = document.getElementById("username");
 var password = document.getElementById("password");
 var error = document.querySelectorAll(".error");
 
-// set event listener for login form submission
+// add event listener for login form submission
 loginForm.addEventListener('submit', function(event) {
     // stop the event from its default action: submitting the form (for our validation, submission is not desired)
     event.preventDefault();
@@ -19,19 +19,19 @@ loginForm.addEventListener('submit', function(event) {
 
     // username should not be empty
     if (validator.isEmpty(userName.value)) {
-      error[0].innerHTML = "Hey Buddy! Even bacon lovers need to enter a user name.";
-      error[0].className = "error active";
-      userName.className = "invalid";
-    // username should not have any extra whitespace
+        error[0].innerHTML = "Hey Buddy! Even bacon lovers need to enter a user name.";
+        error[0].className = "error active";
+        userName.className = "invalid";
+        // username should not have any extra whitespace
     } else if (!validator.isTrimmed(userName.value)) {
-      error[0].innerHTML = "Ummm... Your user name shouldn't have any extra whitespace added.";
-      error[0].className = "error active";
-      userName.className = "invalid";
-    // username should be alphanumeric
+        error[0].innerHTML = "Ummm... Your user name shouldn't have any extra whitespace added.";
+        error[0].className = "error active";
+        userName.className = "invalid";
+        // username should be alphanumeric
     } else if (!validator.isAlphanumeric(userName.value)) {
-      error[0].innerHTML = "Whoa there... Your user name shouldn't have any special characters in it.";
-      error[0].className = "error active";
-      userName.className = "invalid";
+        error[0].innerHTML = "Whoa there... Your user name shouldn't have any special characters in it.";
+        error[0].className = "error active";
+        userName.className = "invalid";
     }
 
     /*---------------------------------
@@ -40,19 +40,39 @@ loginForm.addEventListener('submit', function(event) {
 
     // password should not be empty
     if (validator.isEmpty(password.value)) {
-      error[1].innerHTML = "Oops! Thoughts of bacon must have made you forget to enter a password.";
-      error[1].className = "error active";
-      password.className = "invalid";
-    // password should not have any extra whitespace
+        error[1].innerHTML = "Oops! Thoughts of bacon must have made you forget to enter a password.";
+        error[1].className = "error active";
+        password.className = "invalid";
+        // password should not have any extra whitespace
     } else if (!validator.isTrimmed(password.value)) {
-      error[1].innerHTML = "Ummm... Your password shouldn't have any extra whitespace added.";
-      error[1].className = "error active";
-      password.className = "invalid";
-    // password should not contain any spaces
+        error[1].innerHTML = "Ummm... Your password shouldn't have any extra whitespace added.";
+        error[1].className = "error active";
+        password.className = "invalid";
+        // password should not contain any spaces
     } else if (!validator.contains(password.value, [' '])) {
-      error[1].innerHTML = "Doh! Your password shouldn't contain any spaces.";
-      error[1].className = "error active";
-      password.className = "invalid";
+        error[1].innerHTML = "Doh! Your password shouldn't contain any spaces.";
+        error[1].className = "error active";
+        password.className = "invalid";
     }
 
+});
+
+// add event listener for input on username field
+userName.addEventListener('input', function(event) {
+  // check for and reset error on input
+    if (error[0].innerHTML !== "") {
+        error[0].innerHTML = "";
+        error[0].className = "error";
+        userName.classList.remove("invalid");
+    }
+});
+
+// add event listener for input on password field
+password.addEventListener('input', function(event) {
+  // check for and reset error on input
+    if (error[1].innerHTML !== "") {
+        error[1].innerHTML = "";
+        error[1].className = "error";
+        password.classList.remove("invalid");
+    }
 });
