7fb1b96c9be68f738acbb7b386d3571b15947bc3
Sam Chittenden <sbchittenden@gmail.com>
2016-09-01 14:08:15 -0600

    Updated address.js

---
+++ ship_and_bill_address/address.js
@@ -0,0 +1,24 @@
+//////////////////////////////////////
+//Shipping and billing address form //
+//////////////////////////////////////
+
+// retrieve form element
+var addressForm = document.getElementById("address_form");
+
+// retrieve shipping elements
+var shipFirstName = document.getElementById("ship_first_name");
+var shipLastName = document.getElementById("ship_last_name");
+var shipAddress = document.getElementById("ship_address");
+var shipAddressLine2 = document.getElementById("ship_address_2");
+var shipCity = document.getElementById("ship_city");
+var shipState = document.getElementById("ship_state");
+var shipCountry = document.getElementById("ship_country");
+
+// retrieve billing elements
+var billFirstName = document.getElementById("bill_first_name");
+var billLastName = document.getElementById("bill_last_name");
+var billAddress = document.getElementById("bill_address");
+var billAddressLine2 = document.getElementById("bill_address_2");
+var billCity = document.getElementById("bill_city");
+var billState = document.getElementById("bill_state");
+var billCountry = document.getElementById("bill_country");
