3577bf21ca4691270f3f279e90ba41af9c785f33
Sam Chittenden <sbchittenden@gmail.com>
2016-09-01 10:24:16 -0600

    Updated search form validation
    Modified index.html
    added .error, .active, and .invalid classes to search.css
    updated validation scripts in search.js

--- search/search.js
+++ search/search.js
@@ -6,47 +6,37 @@
 var searchForm = document.getElementById('search_form');
 // retrieve search input element
 var searchInput = document.getElementById('search');
+// retrieve error class
+var error = document.querySelector('.error');
 
 searchForm.addEventListener('submit', function(event) {
 
     // stop the event from its default action: submitting the form (for our validation, submission is not desired)
     event.preventDefault();
 
-    var input = searchInput.value;
-
     // search input element shouldn't be empty
-    if (validator.isEmpty(input)) {
+    if (validator.isEmpty(searchInput.value)) {
+        error.innerHTML = "Oops! You didn't enter any search terms."
+        error.className = "error active";
+        searchInput.className = "invalid";
+    } else if (!validator.isTrimmed(searchInput.value)) { // search input element should be trimmed (no leading or trailing whitespace)
+        error.innerHTML = "Doh! Please don't include any extra spaces."
+        error.className = "error active";
         searchInput.className = "invalid";
     } else {
-        searchInput.className = "valid";
-    }
-
-    // search input element should be trimmed (no leading or trailing whitespace)
-    if (!validator.isTrimmed(input)) {
-        searchInput.className = "invalid";
-    } else {
-        searchInput.className = "valid";
+        searchForm.className = "valid";
     }
 
 });
 
 // add eventListener for form focus and add valid class if input validates
-// searchInput.addEventListener('blur', function(event) {
+searchInput.addEventListener('input', function(event) {
 
-//     var input = searchInput.value.trim();
+    // check for and reset error on input
+    if (error.innerHTML !== "") {
+        error.innerHTML = "";
+        error.className = "error";
+        searchInput.className = "";
+    }
 
-//     // search input element shouldn't be empty
-//     if (validator.isEmpty(input)) {
-//         searchInput.className = "invalid";
-//     } else {
-//         searchInput.className = "valid";
-//     }
-
-//     // search input element should be trimmed (no leading or trailing whitespace)
-//     if (!validator.isTrimmed(input)) {
-//         searchInput.className = "invalid";
-//     } else {
-//         searchInput.className = "valid";
-//     }
-
-// });
+});
