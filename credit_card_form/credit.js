e9c8466ea2455f93c37797bbaccb4b5f689a3ad1
Samuel Chittenden <sbchittenden@gmail.com>
2016-09-02 22:08:31 -0600

    Updated credit.js and credit card index.html
    Added an expiration date validation

--- credit_card_form/credit.js
+++ credit_card_form/credit.js
@@ -24,12 +24,12 @@ ccForm.addEventListener('submit', function(event) {
     --------------------------------------------*/
     // full name should not be empty
     if (validator.isEmpty(fullName.value)) {
-        error[0].innerHTML = "Oops! Please enter your full name."
+        error[0].innerHTML = "Oops! Please enter your full name.";
         error[0].className = "error active";
         fullName.parentNode.classList.add("invalid");
         // full name should not have extra whitespace
     } else if (!validator.isTrimmed(fullName.value)) {
-        error[0].innerHTML = "Please don't include any extra whitespace."
+        error[0].innerHTML = "Please don't include any extra whitespace.";
         error[0].className = "error active";
         fullName.parentNode.classList.add("invalid");
     } else {
@@ -43,12 +43,12 @@ ccForm.addEventListener('submit', function(event) {
 
     // cc number field should not be empty
     if (validator.isEmpty(creditCardNumber.value)) {
-        error[1].innerHTML = "Oh oh! Please enter a card number."
+        error[1].innerHTML = "Oh oh! Please enter a card number.";
         error[1].className = "error active";
         creditCardNumber.parentNode.classList.add("invalid");
         // input should pass isCreditCard validation
     } else if (!validator.isCreditCard(creditCardNumber.value)) {
-        error[1].innerHTML = "Doh! Your card number doesn't look right..."
+        error[1].innerHTML = "Doh! Your card number doesn't look right...";
         error[1].className = "error active";
         creditCardNumber.parentNode.classList.add("invalid");
     } else {
@@ -62,12 +62,12 @@ ccForm.addEventListener('submit', function(event) {
 
     // cvc code should not be empty
     if (validator.isEmpty(cvcCode.value)) {
-        error[2].innerHTML = "Whoops. Please enter your CVC code."
+        error[2].innerHTML = "Whoops. Please enter your CVC code.";
         error[2].className = "error active";
         cvcCode.parentNode.classList.add("invalid");
         // cvc code should be composed of only numbers
     } else if (!validator.isComposedOf(cvcCode.value, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])) {
-        error[2].innerHTML = "Ahem... Your CVC code should only contain numbers."
+        error[2].innerHTML = "Ahem... Your CVC code should only contain numbers.";
         error[2].className = "error active";
         cvcCode.parentNode.classList.add("invalid");
     } else {
@@ -78,12 +78,21 @@ ccForm.addEventListener('submit', function(event) {
     /*-------------------------------------------
     validation for expiration date options  
     ----------------------------------------------*/
+    var month = expMonth.value;
+    var year = expYear.value;
 
     // expiration date should not be set to 'month' or 'year'
-    if (expMonth.value === "month" || expYear.value === "year") {
+    if (month === "month" || year === "year") {
         error[3].innerHTML = "Please select your expiration date";
         error[3].className = "error active";
         expMonth.parentNode.classList.add("invalid");
+        // expiration date should be after today    
+    } else if (month !== "month" && year !== "year") {
+        if (!validator.isAfterToday(new Date(+year, (+month + 1)))) {
+            error[3].innerHTML = "Oops! Looks like that card may be expired";
+            error[3].className = "error active";
+            expMonth.parentNode.classList.add("invalid");
+        }
     } else {
         expMonth.parentNode.classList.remove("invalid");
         ccForm.className = "valid";
@@ -133,15 +142,10 @@ expYear.addEventListener('input', function(event) {
 
 // add event listener for input on expiry month 
 expMonth.addEventListener('input', function(event) {
-   // check for and reset error on input
+    // check for and reset error on input
     if (error[3].innerHTML !== "") {
         error[3].innerHTML = "";
         error[3].className = "error";
         expMonth.parentNode.classList.remove("invalid");
-    } 
+    }
 });
-
-
-
-
-
